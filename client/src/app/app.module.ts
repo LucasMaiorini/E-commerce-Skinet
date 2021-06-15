49e470565e02ed6e178669e6e4bf28c70f8fa2f1
LucasMaiorini <lucasmaiorini@gmai.com>
2021-06-15 00:37:06 -0300

    Git environment setted.

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  // Apply bootstrap in our AppComponent
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
